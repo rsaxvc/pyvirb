36c722abdd1f94670d6bef68d974ffd48e3c91c6
Richard Allen <rsaxvc@rsaxvc.net>
2015-12-16 22:33:05 -0600

    shuffle some code around

--- console.py
+++ console.py
@@ -2,20 +2,7 @@ import virb_list
 import virb
 
 from six.moves import input
-from zeroconf import ServiceBrowser, Zeroconf
 
-zeroconf = Zeroconf()
-virb_mdns_key = "_garmin-virb._tcp.local."
-browser = ServiceBrowser(zeroconf, virb_mdns_key, virb_list.VirbListener() )
-try:
-	host = input("Scanning for VIRBs. Please enter one...\n")
-	if( virb_mdns_key not in host ):
-		host = host + "." + virb_mdns_key
-	v = virb.Virb(host)
-finally:
-    zeroconf.close()
-
-
-
-
-
+l = virb_list.VirbLister()
+host = input("Scanning for VIRBs. Please enter one...\n")
+v = virb.Virb(host)
--- virb_list.py
+++ virb_list.py
@@ -1,18 +1,27 @@
-from zeroconf import Zeroconf
+from zeroconf import ServiceBrowser, Zeroconf
 
-class VirbListener(object):
-	virbs = {}
-	def remove_service(self, zeroconf, type, name):
-		if( name in self.virbs ):
-			del self.virbs[name]
-			print("VIRB %s left" % (name,))
+class VirbLister(object):
+	virb_mdns_key = "_garmin-virb._tcp.local."
 
-	def add_service(self, zeroconf, type, name):
-		info = zeroconf.get_service_info(type, name)
-		if( name not in self.virbs ):
-			self.virbs[name] = info
-			print("VIRB %s found" % (name,))
-	def virb_list(self):
-		for virb in self.virbs:
-			yield virb
+	class VirbListener(object):
+		virbs = {}
+		def remove_service(self, zeroconf, type, name):
+			if( name in self.virbs ):
+				del self.virbs[name]
+				print("VIRB %s left" % (name,))
 
+		def add_service(self, zeroconf, type, name):
+			info = zeroconf.get_service_info(type, name)
+			if( name not in self.virbs ):
+				self.virbs[name] = info
+				print("VIRB %s found" % (name,))
+		def virb_list(self):
+			for virb in self.virbs:
+				yield virb
+
+	def __init__(self):
+		self.zeroconf = Zeroconf()
+		self.listener = self.VirbListener()
+		self.browser = ServiceBrowser(self.zeroconf, self.virb_mdns_key, self.listener )
+	def __del__(self):
+		self.zeroconf.close();
